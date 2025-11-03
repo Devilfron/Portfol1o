43f57839ee03c0765ff9c25cc0e0a76c4e32e9e0
Devilfron <inhagni@gmail.com>
2025-11-03 22:11:28 +0100

    initializace Swiper

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 new WOW().init();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
