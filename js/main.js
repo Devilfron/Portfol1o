8ecfa3bb39523bfb8e49fced665f9fd01bf476f3
Devilfron <inhagni@gmail.com>
2025-11-04 10:18:04 +0100

    edit slider and bullet

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  speed: 400,
+  spacebetween: 100,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.projects-pagination',
+    type: 'bullets',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true,
   },
 
 });
